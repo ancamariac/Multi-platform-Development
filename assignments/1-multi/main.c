d0296aa0a33e2642d8424901491faa624deec753
ancamariac <ciuciuancamaria@gmail.com>
2022-03-15 16:13:13 +0200

    Parsed arguments

--- assignments/1-multi/main.c
+++ assignments/1-multi/main.c
@@ -1,18 +1,78 @@
 #include "helper.h"
- 
+
 int main(int argc, char **argv)
 {
-   // printf("%d", hashFunc("Hello2", 10));
-
+	char *inputFileName = NULL;
+	char *outputFileName = NULL;
 	HashMap map = createHashMap(6);
-	insert(&map, "key", "val");
-	delete (&map, "key");
-	insert(&map, "key", "value");
 
-	char *res = get(&map, "key");
-	printf("%s\n", res);
+   FILE *inFile = fopen(inputFileName, "r");
+   FILE *outFile = fopen(outputFileName, "w");
 
-   deleteMap(&map);
+   if (inFile == NULL) {
+      exit(EXIT_FAILURE);
+   }
 
-   return 0;
+    if (outFile == NULL) {
+      exit(EXIT_FAILURE);
+   }
+
+	for (int i = 0; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			if (argv[i][1] == 'D') {
+				if (strlen(argv[i]) == 2) {
+					if (argv[i + 2] &&
+					    argv[i + 1][0] != '-') {
+						insert(&map, argv[i + 1],
+						       argv[i + 2]);
+					} else {
+						insert(&map, argv[i + 1], "");
+						i--;
+					}
+					i += 2;
+				} else {
+					
+					if (argv[i + 1] && argv[i + 1][0] != '-') {
+						insert(&map, argv[i] + 2, argv[i + 1]);
+						
+					} else {
+						insert(&map, argv[i] + 2, "");
+						i--;
+					}
+					i += 1;
+				}
+			} else if (argv[i][1] == 'I') {
+				// to do
+			} else if (argv[i][1] == 'o') {
+				if (strlen(argv[i]) == 2) {
+               outputFileName = malloc((strlen(argv[i + 1]) + 1) * sizeof(char));
+					strcpy(outputFileName, argv[i + 1]);
+					i++;
+				} else {
+               		outputFileName = malloc((strlen(argv[i] + 2) + 1) * sizeof(char));
+					strcpy(outputFileName, argv[i] + 2);
+				}
+			} 
+		}
+		else {
+            	inputFileName = malloc((strlen(argv[i]) + 1) * sizeof(char));
+				strcpy(inputFileName, argv[i]);
+			}
+	}
+	printf("%s\n", inputFileName);
+	printf("%s\n", outputFileName);
+	printf("%s\n", get(&map, "var0"));
+
+   char ch;
+
+   while ((ch = fgetc(inFile)) != EOF) {
+      fputc(ch, outFile);
+   }
+
+   fclose(inFile);
+   fclose(outFile);
+
+	deleteMap(&map);
+
+	return 0;
 }
