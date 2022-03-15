ae9be73d41ab181eb8f2adfe4029305415a03c03
ancamariac <ciuciuancamaria@gmail.com>
2022-03-15 16:36:28 +0200

    Files in main

--- assignments/1-multi/main.c
+++ assignments/1-multi/main.c
@@ -6,16 +6,16 @@ int main(int argc, char **argv)
 	char *outputFileName = NULL;
 	HashMap map = createHashMap(6);
 
-   FILE *inFile = fopen(inputFileName, "r");
-   FILE *outFile = fopen(outputFileName, "w");
+	FILE *inFile = fopen(inputFileName, "r");
+	FILE *outFile = fopen(outputFileName, "w");
 
-   if (inFile == NULL) {
-      exit(EXIT_FAILURE);
-   }
+	if (inFile == NULL) {
+		exit(EXIT_FAILURE);
+	}
 
-    if (outFile == NULL) {
-      exit(EXIT_FAILURE);
-   }
+	if (outFile == NULL) {
+		exit(EXIT_FAILURE);
+	}
 
 	for (int i = 0; i < argc; i++) {
 		if (argv[i][0] == '-') {
@@ -31,10 +31,12 @@ int main(int argc, char **argv)
 					}
 					i += 2;
 				} else {
-					
-					if (argv[i + 1] && argv[i + 1][0] != '-') {
-						insert(&map, argv[i] + 2, argv[i + 1]);
-						
+
+					if (argv[i + 1] &&
+					    argv[i + 1][0] != '-') {
+						insert(&map, argv[i] + 2,
+						       argv[i + 1]);
+
 					} else {
 						insert(&map, argv[i] + 2, "");
 						i--;
@@ -45,34 +47,52 @@ int main(int argc, char **argv)
 				// to do
 			} else if (argv[i][1] == 'o') {
 				if (strlen(argv[i]) == 2) {
-               outputFileName = malloc((strlen(argv[i + 1]) + 1) * sizeof(char));
+					outputFileName =
+					    malloc((strlen(argv[i + 1]) + 1) *
+						   sizeof(char));
 					strcpy(outputFileName, argv[i + 1]);
 					i++;
 				} else {
-               		outputFileName = malloc((strlen(argv[i] + 2) + 1) * sizeof(char));
+					outputFileName =
+					    malloc((strlen(argv[i] + 2) + 1) *
+						   sizeof(char));
 					strcpy(outputFileName, argv[i] + 2);
 				}
-			} 
-		}
-		else {
-            	inputFileName = malloc((strlen(argv[i]) + 1) * sizeof(char));
-				strcpy(inputFileName, argv[i]);
 			}
+		} else {
+			inputFileName =
+			    malloc((strlen(argv[i]) + 1) * sizeof(char));
+			strcpy(inputFileName, argv[i]);
+		}
 	}
+
+   if (!inputFileName) {
+      inputFileName = malloc(strelen("STDIN") * sizeof(char));
+      strcpy(inputFileName, "STDIN");
+   }
+
+   if (!outputFileName) {
+      outputFileName = malloc(strelen("STDOUT") * sizeof(char));
+      strcpy(outputFileName, "STDOUT");
+   }
+
+	char ch;
+
+	while ((ch = fgetc(inFile)) != EOF) {
+		fputc(ch, outFile);
+	}
+
+	fclose(inFile);
+   free(inputFileName);
+
+	fclose(outFile);
+   free(outputFileName);
+   
+	deleteMap(&map);
+   
 	printf("%s\n", inputFileName);
 	printf("%s\n", outputFileName);
 	printf("%s\n", get(&map, "var0"));
-
-   char ch;
-
-   while ((ch = fgetc(inFile)) != EOF) {
-      fputc(ch, outFile);
-   }
-
-   fclose(inFile);
-   fclose(outFile);
-
-	deleteMap(&map);
 
 	return 0;
 }
