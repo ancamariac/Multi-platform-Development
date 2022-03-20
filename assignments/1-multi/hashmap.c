d6e2e128686781f6c6baa16ccec8090142a58573
ancamariac <ciuciuancamaria@gmail.com>
2022-03-20 21:01:06 +0200

    Rearranged files

---
+++ assignments/1-multi/hashmap.c
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "hashmap.h"
+
+#define MAX_SIZE 20
+
+HashMap createHashMap(int size)
+{
+	HashMap newMap;
+
+	int i = 0;
+
+	newMap.size = size;
+	newMap.cnt = 0;
+	newMap.elem = malloc(newMap.size * sizeof(Elem));
+
+	if (!newMap.elem)
+		exit(12);
+
+	for (i = 0; i < newMap.size; i++) {
+		newMap.elem[i].key = NULL;
+		newMap.elem[i].value = NULL;
+	}
+	return newMap;
+}
+
+int hashFunc(char *key, int size)
+{
+	int hash = 0;
+	const char *p = NULL;
+
+	for (p = key; *p; p++)
+		hash += (unsigned char)(*p);
+	return hash % size;
+}
+
+void deleteMap(HashMap *map)
+{
+	int i = 0;
+
+	for (i = 0; i < map->size; i++) {
+		if (map->elem[i].key) {
+			free(map->elem[i].key);
+			free(map->elem[i].value);
+		}
+	}
+	free(map->elem);
+}
+
+void insert(HashMap *map, char *key, char *value)
+{
+	int i = 0;
+	int index = 0;
+
+	if (map->cnt * 4 > map->size * 3) {
+		HashMap newMap = createHashMap(2 * map->size);
+
+		for (i = 0; i < map->size; i++) {
+			if (map->elem[i].key != NULL) {
+				insert(&newMap, map->elem[i].key,
+				       map->elem[i].value);
+			}
+		}
+		deleteMap(map);
+		*map = newMap;
+	}
+
+	index = hashFunc(key, map->size);
+
+	while (1) {
+		if (map->elem[index].key == NULL) {
+			map->elem[index].key = malloc(strlen(key) + 1);
+
+			if (!map->elem[index].key)
+				exit(12);
+
+			map->elem[index].value = malloc(strlen(value) + 1);
+
+			if (!map->elem[index].value)
+				exit(12);
+
+			strcpy(map->elem[index].key, key);
+			strcpy(map->elem[index].value, value);
+			map->cnt++;
+			return;
+		}
+		index += 1;
+		index %= map->size;
+	}
+}
+
+char *get(HashMap *map, char *key)
+{
+	int index = 0;
+
+	index = hashFunc(key, map->size);
+
+	while (1) {
+		if (map->elem[index].key == NULL)
+			return NULL;
+		else if (!strcmp(map->elem[index].key, key))
+			return map->elem[index].value;
+		index += 1;
+		index %= map->size;
+	}
+}
+
+void delete(HashMap *map, char *key)
+{
+	int index = 0;
+
+	index = hashFunc(key, map->size);
+
+	while (1) {
+		if (map->elem[index].key == NULL) {
+			return;
+
+		} else if (!strcmp(map->elem[index].key, key)) {
+			free(map->elem[index].key);
+			free(map->elem[index].value);
+			map->elem[index].key = NULL;
+			map->elem[index].value = NULL;
+
+			return;
+		}
+		index += 1;
+		index %= map->size;
+	}
+}
--- assignments/1-multi/helper.h
+++ assignments/1-multi/helper.h
@@ -3,29 +3,30 @@
 
 #endif
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Elem {
-	char *key;
-	char *value;
-} Elem;
+int getLine(char **line, FILE *in);
 
-typedef struct HashMap {
-	Elem *elem;
-	int cnt;
-	int size;
-} HashMap;
+FILE *getIncFile(char *fileName, char **directories, int numDir,
+		 char *currentDir);
 
-HashMap createHashMap(int size);
+char *replace(char *line, char *key, char *value);
 
-int hashFunc(char *key, int size);
+void ifelse(FILE *in, FILE *out, HashMap *map, int cond, int done);
 
-void deleteMap(HashMap *map);
+void ifdef(FILE *in, FILE *out, HashMap *map, int cond);
 
-void insert(HashMap *map, char *key, char *value);
+char *getDirectory(char *path);
 
-char *get(HashMap *map, char *key);
+char *concatenate(char *dest, const char *src);
 
-void delete(HashMap *map, char *key);
+void check_token(char *token, char **res);
+
+void parseFile(FILE *in, FILE *out, HashMap *map, char **directories,
+	       int numDir, char *inFileName);
+
+char **getArgs(int argc, char **argv, char **input, char **output,
+HashMap *map, int *numDir);
