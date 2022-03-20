7c494fcc2bb90e3c56a7761174811d27c10ee846
ancamariac <ciuciuancamaria@gmail.com>
2022-03-20 21:06:41 +0200

    attempt to fix includes

--- assignments/1-multi/helper.h
+++ assignments/1-multi/helper.h
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "hashmap.h"
 
 int getLine(char **line, FILE *in);
 
--- assignments/1-multi/main.c
+++ assignments/1-multi/main.c
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "hashmap.h"
 #include "helper.h"
 
 int main(int argc, char **argv)
