d4733aea6e146dba368d5c14dd0884dfe3e323a5
ancamariac <ciuciuancamaria@gmail.com>
2022-03-28 23:08:14 +0300

    Added fopen

--- assignments/2-stdio/checker-lin/libso_stdio.c
+++ assignments/2-stdio/checker-lin/libso_stdio.c
@@ -1,1 +1,30 @@
 #include "so_stdio.h"
+#include <fcntl.h>
+
+SO_FILE *so_fopen(const char *pathname, const char *mode) {
+
+    int fd = -1;
+    int cursor = 0;
+
+    if (strcmp(mode, "r") == 0)
+        fd = open(pathname, O_RDONLY, 0666);
+    else if (strcmp(mode, "r+") == 0)
+        fd = open(pathname, O_RDWR, 0666);
+    else if (strcmp(mode, "w") == 0)
+        fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    else if (strcmp(mode, "w+") == 0)
+        fd = open(pathname, O_RDWR | O_CREAT | O_TRUNC, 0666);
+    else if (strcmp(mode, "a") == 0)
+        fd = open(pathname, O_WRONLY | O_CREAT | O_APPEND, 0666);
+    else if (strcmp(mode, "a+") == 0)
+        fd = open(pathname, O_RDWR | O_CREAT | O_APPEND, 0666);
+    else
+        return NULL;
+
+    SO_FILE *file = malloc(sizeof(SO_FILE));
+
+    file->fd = fd;
+    file->cursor = cursor;
+
+    return file;
+}
