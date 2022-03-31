0f0fedba5ca83086cf6a317701a378036c559d33
ancamariac <ciuciuancamaria@gmail.com>
2022-03-31 23:33:18 +0300

    solved error I hope

--- assignments/2-stdio/checker-lin/libso_stdio.c
+++ assignments/2-stdio/checker-lin/libso_stdio.c
@@ -190,9 +190,17 @@ size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
 {
     size_t cnt = 0;
     long var = 0;
+    unsigned char character;
+    int char_pos = 0;
 
     while (cnt < size * nmemb) {
-        var = so_fputc(stream);
+        character = ptr[cnt + char_pos];
+        var = so_fputc(character, stream);
+
+        char_pos++;
+
+        if (char_pos == size)
+            break;
 
         if (var == SO_EOF)
             break;
